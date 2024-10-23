8f02977cedc743c9527163c18e82dfa478c615d1
Ckyong77 <110823542+Ckyong77@users.noreply.github.com>
2024-10-23 13:12:11 +0800

    Update users.js

--- back-end/seeds/users.js
+++ back-end/seeds/users.js
@@ -1,4 +1,4 @@
 module.exports = [
     "Alice", "Bob", "Charlie", "Diana", "Ethan", "Fiona", "George", "Hannah",
-    "Isaac", "Jack", "Karen", "Liam"
+    "Isaac", "Jack", "Karen", "Liam", "Jackson"
 ]
